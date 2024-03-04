7711b71d0dbbe6bf0768d449ea5df066667c3ffa
Jacobus Geluk <jacobus.geluk@ekgf.org>
2024-03-04 09:33:47 +0000

    chore: bump up terraform and aws versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.7.4"
 
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 5.22.0"
+      version               = ">= 5.39.0"
       configuration_aliases = [aws.neptune]
     }
   }
